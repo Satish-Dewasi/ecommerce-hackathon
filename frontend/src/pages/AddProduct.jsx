cb16b55fdf27312c2c046d9434fbd542392be873
Satish Dewasi <satishdewasi101@gmail.com>
2026-04-09 01:03:37 +0530

    test

--- frontend/src/pages/AddProduct.jsx
+++ frontend/src/pages/AddProduct.jsx
@@ -1,6 +1,6 @@
 import ProductForm from "@/components/ProductForm";
 
-// This page is used to add a new product
+// This page is used to add a new product and reuses the ProductForm component in "add" mode.
 const AddProduct = () => <ProductForm mode="add" />;
 
 export default AddProduct;
